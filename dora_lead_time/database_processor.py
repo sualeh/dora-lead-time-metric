8967759452f13b5f47e5bdc5044a44335f3529c7
Sualeh Fatehi <sualeh@hotmail.com>
2025-05-04 22:14:18 -0400

    Externalize schema creation script

--- dora_lead_time/database_processor.py
+++ dora_lead_time/database_processor.py
@@ -2,6 +2,7 @@
 
 import logging
 import os
+import pathlib
 from datetime import date, datetime
 import sqlite3
 from dotenv import load_dotenv
@@ -148,111 +149,17 @@ class DatabaseProcessor:
             conn = self._get_connection()
             cursor = conn.cursor()
 
-            schema_script = """
-                CREATE TABLE IF NOT EXISTS projects (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    project_internal_id VARCHAR(1024),
-                    project_key VARCHAR(1024),
-                    project_title VARCHAR(1024),
-                    project_type VARCHAR(1024),
-                    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-                    UNIQUE(project_internal_id),
-                    UNIQUE(project_key)
-                );
+            schema_dir = pathlib.Path(__file__).parent / "schema"
+            schema_dir.mkdir(exist_ok=True)
+            schema_script_path = schema_dir / "schema.sql"
+            if not schema_script_path.exists():
+                raise FileNotFoundError(
+                    f"SQL file {schema_script_path} not found"
+                )
 
-                CREATE TABLE IF NOT EXISTS releases (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    release_internal_id VARCHAR(1024),
-                    release_title VARCHAR(1024),
-                    release_description VARCHAR(2048),
-                    release_date VARCHAR(1024), -- DATE
-                    project_id INTEGER,
-                    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-                    UNIQUE(release_internal_id, project_id),
-                    FOREIGN KEY (project_id) REFERENCES projects (id)
-                );
-
-                CREATE TABLE IF NOT EXISTS stories (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    story_key VARCHAR(1024),
-                    story_title VARCHAR(1024),
-                    story_type VARCHAR(1024),
-                    story_created VARCHAR(1024), -- DATE
-                    story_resolved VARCHAR(1024), -- DATE
-                    release_id VARCHAR(1024),
-                    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-                    UNIQUE(story_key, release_id),
-                    FOREIGN KEY (release_id) REFERENCES releases (id)
-                );
-
-                CREATE TABLE IF NOT EXISTS pull_requests (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    pr_title VARCHAR(1024),
-                    pr_owner VARCHAR(1024),
-                    pr_repository VARCHAR(1024),
-                    pr_number VARCHAR(1024),
-                    pr_open VARCHAR(1024), -- DATE
-                    pr_close VARCHAR(1024), -- DATE
-                    commit_count INTEGER,
-                    earliest_commit_date VARCHAR(1024), -- DATE
-                    latest_commit_date VARCHAR(1024), -- DATE
-                    pr_url VARCHAR(1024) GENERATED ALWAYS
-                      AS (
-                        'https://github.com/' || pr_owner || '/' ||
-                        pr_repository || '/pull/' || pr_number
-                      ) VIRTUAL,
-                    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-                    UNIQUE(pr_owner, pr_repository, pr_number)
-                );
-
-                CREATE TABLE IF NOT EXISTS stories_pull_requests (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    story_id INTEGER,
-                    pr_id INTEGER,
-                    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-                    UNIQUE(story_id, pr_id),
-                    FOREIGN KEY (story_id) REFERENCES stories (id),
-                    FOREIGN KEY (pr_id) REFERENCES pull_requests (id)
-                );
-
-                CREATE TABLE IF NOT EXISTS stories_pull_request_counts (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    story_key VARCHAR(1024),
-                    pr_count INTEGER,
-                    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-                    UNIQUE(story_key),
-                    FOREIGN KEY (story_key) REFERENCES stories (story_key)
-                );
-
-                DROP VIEW IF EXISTS lead_times;
-
-                CREATE VIEW lead_times AS
-                SELECT
-                    releases.id
-                    AS release_id,
-                    releases.release_date,
-                    projects.project_key,
-                    pull_requests.id
-                    AS pr_id,
-                    pull_requests.pr_title,
-                    pull_requests.pr_owner,
-                    pull_requests.pr_repository,
-                    pull_requests.pr_number,
-                    pull_requests.earliest_commit_date,
-                    julianday(releases.release_date) -
-                      julianday(pull_requests.earliest_commit_date) + 1
-                      AS lead_time
-                FROM
-                    releases
-                    JOIN projects
-                    ON releases.project_id = projects.id
-                    JOIN stories
-                    ON stories.release_id = releases.id
-                    JOIN stories_pull_requests
-                    ON stories_pull_requests.story_id = stories.id
-                    JOIN pull_requests
-                    ON stories_pull_requests.pr_id = pull_requests.id;
-            """
+            schema_script = ""
+            with open(schema_script_path, "r", encoding="utf-8") as f:
+                schema_script = f.read()
 
             cursor.executescript(schema_script)
             conn.commit()
---
+++ dora_lead_time/schema/schema.sql
@@ -0,0 +1,103 @@
+CREATE TABLE IF NOT EXISTS projects (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	project_internal_id VARCHAR(1024),
+	project_key VARCHAR(1024),
+	project_title VARCHAR(1024),
+	project_type VARCHAR(1024),
+	created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+	UNIQUE(project_internal_id),
+	UNIQUE(project_key)
+);
+
+CREATE TABLE IF NOT EXISTS releases (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	release_internal_id VARCHAR(1024),
+	release_title VARCHAR(1024),
+	release_description VARCHAR(2048),
+	release_date VARCHAR(1024), -- DATE
+	project_id INTEGER,
+	created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+	UNIQUE(release_internal_id, project_id),
+	FOREIGN KEY (project_id) REFERENCES projects (id)
+);
+
+CREATE TABLE IF NOT EXISTS stories (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	story_key VARCHAR(1024),
+	story_title VARCHAR(1024),
+	story_type VARCHAR(1024),
+	story_created VARCHAR(1024), -- DATE
+	story_resolved VARCHAR(1024), -- DATE
+	release_id VARCHAR(1024),
+	created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+	UNIQUE(story_key, release_id),
+	FOREIGN KEY (release_id) REFERENCES releases (id)
+);
+
+CREATE TABLE IF NOT EXISTS pull_requests (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	pr_title VARCHAR(1024),
+	pr_owner VARCHAR(1024),
+	pr_repository VARCHAR(1024),
+	pr_number VARCHAR(1024),
+	pr_open VARCHAR(1024), -- DATE
+	pr_close VARCHAR(1024), -- DATE
+	commit_count INTEGER,
+	earliest_commit_date VARCHAR(1024), -- DATE
+	latest_commit_date VARCHAR(1024), -- DATE
+	pr_url VARCHAR(1024) GENERATED ALWAYS
+	  AS (
+		'https://github.com/' || pr_owner || '/' ||
+		pr_repository || '/pull/' || pr_number
+	  ) VIRTUAL,
+	created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+	UNIQUE(pr_owner, pr_repository, pr_number)
+);
+
+CREATE TABLE IF NOT EXISTS stories_pull_requests (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	story_id INTEGER,
+	pr_id INTEGER,
+	created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+	UNIQUE(story_id, pr_id),
+	FOREIGN KEY (story_id) REFERENCES stories (id),
+	FOREIGN KEY (pr_id) REFERENCES pull_requests (id)
+);
+
+CREATE TABLE IF NOT EXISTS stories_pull_request_counts (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	story_key VARCHAR(1024),
+	pr_count INTEGER,
+	created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+	UNIQUE(story_key),
+	FOREIGN KEY (story_key) REFERENCES stories (story_key)
+);
+
+DROP VIEW IF EXISTS lead_times;
+
+CREATE VIEW lead_times AS
+SELECT
+	releases.id
+	AS release_id,
+	releases.release_date,
+	projects.project_key,
+	pull_requests.id
+	AS pr_id,
+	pull_requests.pr_title,
+	pull_requests.pr_owner,
+	pull_requests.pr_repository,
+	pull_requests.pr_number,
+	pull_requests.earliest_commit_date,
+	julianday(releases.release_date) -
+	  julianday(pull_requests.earliest_commit_date) + 1
+	  AS lead_time
+FROM
+	releases
+	JOIN projects
+	ON releases.project_id = projects.id
+	JOIN stories
+	ON stories.release_id = releases.id
+	JOIN stories_pull_requests
+	ON stories_pull_requests.story_id = stories.id
+	JOIN pull_requests
+	ON stories_pull_requests.pr_id = pull_requests.id;
